cd1f8ec8a3acbc28a153dc445d9bc610649af74a
George Brocklehurst <george@georgebrock.com>
2014-07-05 09:44:30 +0200

    Working C LineEditor delegating to Readline.
    Replaces the Ruby Readline module with a Gitsh::LineEditor module defined in
    a C extension. Currently the LineEditor module just calls methods on the
    Readline module, but eventually that can change.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'bourne'
 require 'parslet/rig/rspec'
 require 'pry'
 
+$LOAD_PATH.unshift(File.expand_path('../../ext', __FILE__))
+
 Dir[File.expand_path('../support/**/*.rb', __FILE__)].each do |path|
   require path
 end
