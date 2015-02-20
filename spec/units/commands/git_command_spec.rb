77b0d0b0c5ee410421f57b560427d6f4dfa50624
George Brocklehurst and Damian Galarza <george+damian@thoughtbot.com>
2015-02-20 17:14:41 -0500

    Refactor: composition over inheritance.
    Refactors GitCommand to use the ShellCommentRunner directly, instead of
    inheriting from ShellCommand.
    
    This paves the way to modify the behaviour of ShellCommand without modifying
    the behaviour of GitCommand.

--- spec/units/commands/git_command_spec.rb
+++ spec/units/commands/git_command_spec.rb
@@ -2,43 +2,34 @@ require 'spec_helper'
 require 'gitsh/commands/git_command'
 
 describe Gitsh::Commands::GitCommand do
-  let(:env) do
-    double('Environment', {
-      git_command: '/usr/bin/env git',
-      output_stream: double('OutputStream', to_i: 1),
-      error_stream: double('ErrorStream', to_i: 2)
-    })
-  end
-
-  before do
-    allow(Process).to receive(:spawn).and_return(1)
-    allow(Process).to receive(:wait)
-  end
-
   describe '#execute' do
-    it 'spawns a process with the sub command and arguments' do
-      allow(env).to receive(:config_variables).and_return({})
-      command = described_class.new(
+    it 'delegates to the Gitsh::ShellCommandRunner' do
+      env = double(:env, git_command: '/usr/bin/env git', config_variables: {})
+      expected_result = double(:result)
+      mock_runner = double(:shell_command_runner, run: expected_result)
+      allow(Gitsh::ShellCommandRunner).to receive(:new).and_return(mock_runner)
+
+      command = described_class.new(env, "commit", arguments("-m", "Some stuff"))
+      result = command.execute
+
+      expect(Gitsh::ShellCommandRunner).to have_received(:new).with(
+        ["/usr/bin/env", "git", "commit", "-m", "Some stuff"],
         env,
-        'commit',
-        arguments('-m', 'A test commit'),
       )
-
-      command.execute
-
-      expect(Process).to have_received(:spawn).with(
-        '/usr/bin/env', 'git',
-        'commit',
-        '-m', 'A test commit',
-        out: env.output_stream.to_i,
-        err: env.error_stream.to_i
-      )
+      expect(mock_runner).to have_received(:run)
+      expect(result).to eq expected_result
     end
 
     it 'passes on configuration variables from the environment' do
-      allow(env).to receive(:config_variables).and_return(
-        :'test.example' => 'This is an example',
-        :'foo.bar' => '1'
+      mock_runner = double(:shell_command_runner, run: true)
+      allow(Gitsh::ShellCommandRunner).to receive(:new).and_return(mock_runner)
+      env = double(
+        :env,
+        git_command: '/usr/bin/env git',
+        config_variables: {
+          :'test.example' => 'This is an example',
+          :'foo.bar' => '1',
+        },
       )
       command = described_class.new(
         env,
@@ -48,14 +39,15 @@ describe Gitsh::Commands::GitCommand do
 
       command.execute
 
-      expect(Process).to have_received(:spawn).with(
-        '/usr/bin/env', 'git',
-        '-c', 'test.example=This is an example',
-        '-c', 'foo.bar=1',
-        'commit',
-        '-m', 'A test commit',
-        out: env.output_stream.to_i,
-        err: env.error_stream.to_i
+      expect(Gitsh::ShellCommandRunner).to have_received(:new).with(
+        [
+          '/usr/bin/env', 'git',
+          '-c', 'test.example=This is an example',
+          '-c', 'foo.bar=1',
+          'commit',
+          '-m', 'A test commit',
+        ],
+        env,
       )
     end
   end
